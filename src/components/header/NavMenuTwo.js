3dd3d3cbe7fa66bcc8c8e3cd779f5595035c4c1a
Sergey Lem <sergey.lem@gmail.com>
2020-02-06 16:52:42 +0600

    Initialization

---
+++ src/components/header/NavMenuTwo.js
@@ -0,0 +1,233 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Link } from "react-router-dom";
+import { multilanguage } from "redux-multilanguage";
+
+const NavMenu = ({ strings, menuWhiteClass, sidebarMenu }) => {
+  return (
+    <div
+      className={` ${
+        sidebarMenu
+          ? "sidebar-menu"
+          : `main-menu ${menuWhiteClass ? menuWhiteClass : ""}`
+      } `}
+    >
+      <nav>
+        <ul>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/"}>
+              {strings["home"]}
+              {/* {sidebarMenu ? (
+                <span>
+                  <i className="fa fa-angle-right"></i>
+                </span>
+              ) : (
+                <i className="fa fa-angle-down" />
+              )} */}
+            </Link>
+          </li>
+
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+              {" "}
+              {strings["shop"]}
+              {sidebarMenu ? (
+                <span>
+                  <i className="fa fa-angle-right"></i>
+                </span>
+              ) : (
+                <i className="fa fa-angle-down" />
+              )}
+            </Link>
+            <ul className="mega-menu">
+              <li>
+                <ul>
+                  <li className="mega-menu-title">  
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+                      {strings["shop_products"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+                      {strings["shop_smartphone"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-filter"}>
+                      {strings["shop_tablet"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-two-column"}>
+                      {strings["shop_wearable"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-no-sidebar"}>
+                      {strings["shop_camera"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-full-width"}>
+                      {strings["shop_laptop"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={process.env.PUBLIC_URL + "/shop-grid-right-sidebar"}
+                    >
+                      {strings["shop_drone"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/shop-list-standard"}>
+                      {strings["shop_accessories"]}
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul>
+                  <li className="mega-menu-title">
+                    <Link to={process.env.PUBLIC_URL + "/product/1"}>
+                      {strings["product_vendors"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/product/1"}>
+                      {strings["product_apple"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/product-tab-left/1"}>
+                      {strings["product_samsung"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/product-tab-right/1"}>
+                      {strings["product_huawei"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/product-sticky/1"}>
+                      {strings["product_xiaomi"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/product-slider/1"}>
+                      {strings["product_meizu"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to={process.env.PUBLIC_URL + "/product-fixed-image/1"}
+                    >
+                      {strings["product_oppo"]}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to={process.env.PUBLIC_URL + "/product/8"}>
+                      {strings["product_zte"]}
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <ul>
+                  <li className="mega-menu-img">
+                    <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+                      <img
+                        src={
+                          process.env.PUBLIC_URL +
+                          "/assets/img/banner/banner-12.png"
+                        }
+                        alt=""
+                      />
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/shop-grid-standard"}>
+              {strings["collection"]}
+            </Link>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/"}>
+              {strings["pages"]}
+              {sidebarMenu ? (
+                <span>
+                  <i className="fa fa-angle-right"></i>
+                </span>
+              ) : (
+                <i className="fa fa-angle-down" />
+              )}
+            </Link>
+            <ul className="submenu">
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/cart"}>
+                  {strings["cart"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/checkout"}>
+                  {strings["checkout"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/wishlist"}>
+                  {strings["wishlist"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/compare"}>
+                  {strings["compare"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/my-account"}>
+                  {strings["my_account"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/login-register"}>
+                  {strings["login_register"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/about"}>
+                  {strings["about_us"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/contact"}>
+                  {strings["contact_us"]}
+                </Link>
+              </li>
+              <li>
+                <Link to={process.env.PUBLIC_URL + "/not-found"}>
+                  {strings["404_page"]}
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to={process.env.PUBLIC_URL + "/contact"}>
+              {strings["contact_us"]}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+NavMenu.propTypes = {
+  menuWhiteClass: PropTypes.string,
+  sidebarMenu: PropTypes.bool,
+  strings: PropTypes.object
+};
+
+export default multilanguage(NavMenu);
