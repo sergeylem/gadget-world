43b9dfbc874d054c8691035958d1fa744b619e3f
Sergey Lem <sergey.lem@gmail.com>
2020-06-25 03:04:36 +0600

    Refactoring

--- src/components/header/NavMenu.js
+++ src/components/header/NavMenu.js
@@ -1,18 +1,20 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { multilanguage } from "redux-multilanguage";
 import { ROOT_URL } from "../../config";
 import { signout, isAuthenticated } from "../../helpers/auth";
 
 const NavMenu = ({ strings, menuWhiteClass, sidebarMenu, history }) => {
+
+  const [path, setPath] = useState(ROOT_URL + "");
   return (
     <div
       className={` ${
         sidebarMenu
           ? "sidebar-menu"
           : `main-menu ${menuWhiteClass ? menuWhiteClass : ""}`
-      } `}
+        } `}
     >
       <nav>
         <ul>
@@ -38,51 +40,64 @@ const NavMenu = ({ strings, menuWhiteClass, sidebarMenu, history }) => {
                   <i className="fa fa-angle-right"></i>
                 </span>
               ) : (
-                <i className="fa fa-angle-down" />
-              )}
+                  <i className="fa fa-angle-down" />
+                )}
             </Link>
             <ul className="mega-menu">
               <li>
                 <ul>
-                  <li className="mega-menu-title">  
+                  <li className="mega-menu-title">
                     <Link to={ROOT_URL + "/shop-grid-standard"}>
                       {strings["shop_products"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/shop-grid-standard"}>
+                    <Link to={ROOT_URL + "/shop-grid-standard"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/smartphone.jpg") }}
+                    >
                       {strings["shop_smartphone"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/shop-grid-filter"}>
+                    <Link to={ROOT_URL + "/shop-grid-filter"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/tablet.jpg") }}
+                    >
                       {strings["shop_tablet"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/shop-grid-two-column"}>
+                    <Link to={ROOT_URL + "/shop-grid-two-column"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/watch.jpg") }}
+                    >
                       {strings["shop_wearable"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/shop-grid-no-sidebar"}>
+                    <Link to={ROOT_URL + "/shop-grid-no-sidebar"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/camera.jpg") }}
+                    >
                       {strings["shop_camera"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/shop-grid-full-width"}>
+                    <Link to={ROOT_URL + "/shop-grid-full-width"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/macbook.jpg") }}
+                    >
                       {strings["shop_laptop"]}
                     </Link>
                   </li>
                   <li>
                     <Link
                       to={ROOT_URL + "/shop-grid-right-sidebar"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/drone.jpg") }}
                     >
                       {strings["shop_drone"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/shop-list-standard"}>
+                    <Link to={ROOT_URL + "/shop-list-standard"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/headphone.jpg") }}
+                    >
                       {strings["shop_accessories"]}
                     </Link>
                   </li>
@@ -96,39 +111,52 @@ const NavMenu = ({ strings, menuWhiteClass, sidebarMenu, history }) => {
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/product/1"}>
+                    <Link to={ROOT_URL + "/product/1"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/logo-apple.jpg") }}
+                      >
                       {strings["product_apple"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/product-tab-left/1"}>
+                    <Link to={ROOT_URL + "/product-tab-left/1"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/logo-samsung.jpg") }}
+                      >
                       {strings["product_samsung"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/product-tab-right/1"}>
+                    <Link to={ROOT_URL + "/product-tab-right/1"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/logo-huawei.jpg") }}
+                      >
                       {strings["product_huawei"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/product-sticky/1"}>
+                    <Link to={ROOT_URL + "/product-sticky/1"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/logo-хiaomi.jpg") }}
+                    >
                       {strings["product_xiaomi"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/product-slider/1"}>
+                    <Link to={ROOT_URL + "/product-slider/1"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/logo-meizu.jpg") }}
+                      >
                       {strings["product_meizu"]}
                     </Link>
                   </li>
                   <li>
                     <Link
                       to={ROOT_URL + "/product-fixed-image/1"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/logo-oppo.jpg") }}
                     >
                       {strings["product_oppo"]}
                     </Link>
                   </li>
                   <li>
-                    <Link to={ROOT_URL + "/product/8"}>
+                    <Link to={ROOT_URL + "/product/8"}
+                      onMouseEnter={() => { setPath(ROOT_URL + "/assets/img/banner/logo-zte.jpg") }}
+                    >
                       {strings["product_zte"]}
                     </Link>
                   </li>
@@ -140,8 +168,7 @@ const NavMenu = ({ strings, menuWhiteClass, sidebarMenu, history }) => {
                     <Link to={ROOT_URL + "/shop-grid-standard"}>
                       <img className="menu-image-one"
                         src={
-                          ROOT_URL +
-                          "/assets/img/banner/banner-1.jpg"
+                          path
                         }
                         alt=""
                       />
@@ -164,8 +191,8 @@ const NavMenu = ({ strings, menuWhiteClass, sidebarMenu, history }) => {
                   <i className="fa fa-angle-right"></i>
                 </span>
               ) : (
-                <i className="fa fa-angle-down" />
-              )}
+                  <i className="fa fa-angle-down" />
+                )}
             </Link>
             <ul className="submenu">
               <li>
@@ -226,7 +253,7 @@ const NavMenu = ({ strings, menuWhiteClass, sidebarMenu, history }) => {
                         history.push("/");
                       })
                     }
-                    >
+                  >
                     {strings["form_logout"]}
                   </Link>
                 ) : (
