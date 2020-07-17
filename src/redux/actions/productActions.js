3dc135ac88d6f186f9eca16b086afdba7fcbe0d4
Sergey Lem <sergey.lem@gmail.com>
2020-07-17 22:50:01 +0600

    Fixed errors with storing files

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,7 +4,9 @@ export const FETCH_PRODUCTS_SUCCESS = "FETCH_PRODUCTS_SUCCESS";
 export const fetchProducts = () => {
   return dispatch => {
     axios
-      .get(process.env.REACT_APP_API_URL + '/products') //Change hardcode URL !!!
+      // .get(process.env.REACT_APP_API_URL + '/products') //Change hardcode URL !!!
+      .get('http://164.90.210.75/api' + '/products') //Change hardcode URL !!!      
+      // .get('http://localhost/api' + '/products') //Change hardcode URL !!!      
       .then(response => {
         dispatch({
           type: FETCH_PRODUCTS_SUCCESS,
